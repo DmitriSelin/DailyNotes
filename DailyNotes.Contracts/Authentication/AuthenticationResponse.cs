edadba50d59aab0bf1c5b5971c338a61afc2bde0
selindimitri@gmail.com <selindimitri@gmail.com>
2022-07-21 14:36:38 +0300

    Add models

---
+++ DailyNotes.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,33 @@
+﻿namespace DailyNotes.Contracts.Authentication
+{
+    public class AuthenticationResponse
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Token { get; set; }
+
+        public AuthenticationResponse(Guid id, string firstName, string lastName, 
+            string email, string password, string token)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            Token = token;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} : {Email}; {Password}";
+        }
+    }
+}
---
+++ DailyNotes.Contracts/Authentication/LoginRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyNotes.Contracts.Authentication
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public LoginRequest(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return $"{Email} : {Password}";
+        }
+    }
+}
