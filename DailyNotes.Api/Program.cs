2537ae63dea48db2d4164be1b50fe8a520f76f53
selindimitri@gmail.com <selindimitri@gmail.com>
2022-07-24 16:23:17 +0300

    Add extension methods

--- DailyNotes.Api/Program.cs
+++ DailyNotes.Api/Program.cs
@@ -3,8 +3,8 @@ using DailyNotes.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
     builder.Services
@@ -20,13 +20,8 @@ var app = builder.Build();
         app.UseSwaggerUI();
     }
 
-    app.Run(async (context) =>
-    {
-        if (context.Request.Path == "/")
-        {
-            context.Response.Redirect("swagger");
-        }
-    });
+    app.UseHttpsRedirection();
+    app.MapControllers();
 
     app.Run();
 }
