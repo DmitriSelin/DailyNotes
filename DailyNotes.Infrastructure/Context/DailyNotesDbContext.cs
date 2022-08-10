b136a43c6fbf9414f666674c599371ac727d5113
selindimitri@gmail.com <selindimitri@gmail.com>
2022-08-10 18:01:19 +0300

    Create UserRepository methods

--- DailyNotes.Infrastructure/Context/DailyNotesDbContext.cs
+++ DailyNotes.Infrastructure/Context/DailyNotesDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DailyNotes.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DailyNotes.Infrastructure.Context
 {
     public class DailyNotesDbContext : DbContext
     {
         public DailyNotesDbContext() => Database.EnsureCreated();
+
+        public DbSet<User> Users => Set<User>();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
--- DailyNotes.Infrastructure/Persistence/UserRepository.cs
+++ DailyNotes.Infrastructure/Persistence/UserRepository.cs
@@ -1,18 +1,30 @@
 ﻿using DailyNotes.Application.Common.Persistence;
 using DailyNotes.Domain.Entities;
+using DailyNotes.Infrastructure.Context;
 
 namespace DailyNotes.Infrastructure.Persistence
 {
     public class UserRepository : IUserRepository
     {
+        private readonly DailyNotesDbContext _dbContext;
+
+        public UserRepository(DailyNotesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public User AddUser(User user)
         {
+            _dbContext.Users.Add(user);
+
+            _dbContext.SaveChanges();
+
             return user;
         }
 
         public User? GetUserByEmail(string email)
         {
-            return new User();
+            return _dbContext.Users.FirstOrDefault(x => x.Email == email);
         }
     }
 }
