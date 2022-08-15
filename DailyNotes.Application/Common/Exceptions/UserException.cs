35b8dc9815dbd05d196e874af05a5d4fff0ac1b7
selindimitri@gmail.com <selindimitri@gmail.com>
2022-08-15 21:43:39 +0300

    Add new User's exception

---
+++ DailyNotes.Application/Common/Exceptions/UserException.cs
@@ -0,0 +1,18 @@
+﻿namespace DailyNotes.Application.Common.Exceptions
+{
+    public class UserException : Exception
+    {
+        public string? MessageForUser { get; }
+
+        public UserException() { }
+
+        public UserException(string message) : base(message) { }
+
+        public UserException(string message, Exception innerException) : base(message, innerException) { }
+
+        public UserException(string message, string messageForUser) : base(message)
+        {
+            MessageForUser = messageForUser;
+        }
+    }
+}
