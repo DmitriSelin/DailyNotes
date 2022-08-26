7b0f0b3e2a1c48a3cc11ca5d6f70012ce8b7a2fb
selindimitri@gmail.com <selindimitri@gmail.com>
2022-08-26 16:27:07 +0300

    Add property to Note entity

---
+++ DailyNotes.Application/Notes/Commands/CreateNoteCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace DailyNotes.Application.Notes.Commands
+{
+    public class CreateNoteCommand : IRequest
+    {
+        public string Name;
+    }
+}
--- DailyNotes.Domain/Entities/Note.cs
+++ DailyNotes.Domain/Entities/Note.cs
@@ -12,6 +12,8 @@
 
         public int LikesCount { get; set; }
 
+        public Guid UserId { get; set; }
+
         public virtual User User { get; set; } = null!;
 
         public Note() { }
